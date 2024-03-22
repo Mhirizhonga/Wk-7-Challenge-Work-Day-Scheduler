a156591c2d2b4acb8f479ada00db5ae010190c41
KodaKing <m.maso@live.co.uk>
2024-03-22 21:55:32 +0000

    Added details to README.md as well as a screenshot of application.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Daily Planner App
+This is a simple calendar application that allows users to add and manage important events for each hour of the day. The app dynamically updates HTML and CSS powered by jQuery and utilizes the Day.js library to handle date and time functionalities.
+
+## User Story
+As an employee with a busy schedule, I want to add important events to a daily planner so that I can manage my time effectively.
+
+## Usage
+Open the planner to see the current day at the top.
+Scroll down to view time blocks for standard business hours.
+Click on a time block to enter an event for that hour.
+Click the save button to save the event in local storage.
+Events will persist between refreshes of the page.
+
+## Technologies Used
+HTML
+CSS
+JavaScript
+jQuery
+Day.js
+
+## Screenshot
+<img src="./assets/images/Week Day Scheduler.png">
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,6 @@ $(document).ready(function()  {
 var currentDay = dayjs().format("dddd, MMMM D, YYYY");
 $("#currentDay").text(currentDay);
 
-//Generate timeblocks for standard business hours (8am - 6pm)
-var businessHours = [];
-for (var i = 8; i <= 17; i++)  {
-    businessHours.push(i);
-    $(".container").append(`
-    <div class="row time-block" id="hour-${i}">
-    <div class="col-1 hour>$(i):00</div>"
-    <textarea class="col-10 description"></textarea>
-    <button class="col-1 saveBtn"><i class="fas fa-save"></i></button>
-    </div>`);
-}
-
 //Colour-code timeblocks based on past, present, and future times
 function updateHourBlocks()  {
     var currentHour = dayjs().hour();
@@ -53,6 +41,6 @@ $(".time-block").each(function()  {
 $(".saveBtn").on("click", function()  {
     var hour = parseInt($(this).parent().attr("id").split("-")[1]);
     var eventText = $(this).siblings(".description").val().trim();
-    localStorage.setitem("event-" + hour, eventText);
+    localStorage.setItem("event-" + hour, eventText);
   })
 });
